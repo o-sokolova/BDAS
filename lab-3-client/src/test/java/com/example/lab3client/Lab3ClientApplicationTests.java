bd8b13162cf98922f6b5c6fafa0225699f8689c8
Соколова Ольга Алексеевна <Olga.Sokolova@bercut.com>
2020-09-30 21:49:58 +0300

    Add lab-3

---
+++ lab-3-client/src/test/java/com/example/lab3client/Lab3ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
